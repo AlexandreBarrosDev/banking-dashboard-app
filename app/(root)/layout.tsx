8082dccba122994cb2b428db79f6c3d1aaf57d8b
Alexandre Barros <alexandrebarros.dev@gmail.com>
2024-10-15 19:58:46 -0300

    :lipstick: feat: incluindo navegação mobile

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
-import Sidebar from '@/components/ui/Sidebar'
+import MobileNav from '@/components/MobileNav';
+import Sidebar from '@/components/Sidebar'
+import Image from 'next/image'
 
 export default function RootLayout({
   children,
@@ -11,7 +13,19 @@ export default function RootLayout({
   return (
     <main className="flex h-screen w-full font-inter">
         <Sidebar user={loggedIn}/>
-        {children}
+        <div className='flex flex-col size-full'>
+          <div className='root-layout'>
+            <Image src='icons/logo2.svg' 
+            width={30} 
+            height={30} 
+            alt='menu icon' 
+            />
+            <div>
+              <MobileNav user={loggedIn} />
+            </div>
+          </div>
+          {children}
+        </div>
     </main>
   );
 }
